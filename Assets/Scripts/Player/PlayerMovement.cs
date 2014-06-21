1d2be970f5827cd30071c6590e64b76e2dffc60b
Blake Bouchard <thatsblake@gmail.com>
2014-06-21 11:05:47 -0600

    Player Beginnings

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float horizontalAcceleration = 1.0f;
+
+	// Use this for initialization
+	void Start()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        //Debug.Log(horizontalAxis);
+        rigidbody2D.AddForce(new Vector2(horizontalAxis * horizontalAcceleration, 0));
+	}
+}
