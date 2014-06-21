ca91e2f836085442f69171d957f9b2c44c344644
sebastienduval <sebastienduval@live.com>
2014-06-21 15:04:16 -0600

    Bubble prefab and some logic modifications.

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -19,6 +19,9 @@ public class Bubble : MonoBehaviour
 	// Update is called once per frame
 	void Update() 
     {
+        AnimatorStateInfo info = _animator.GetCurrentAnimatorStateInfo(0);
+        _currentAnimationTime = info.normalizedTime;
+
         if (_currentState != null)
         {
             _currentState.Run(Time.deltaTime);
@@ -40,20 +43,32 @@ public class Bubble : MonoBehaviour
     #region Statemachine
     private void BeginShoot()
     {
-        _animator.SetTrigger("Pop");
     }
 
     private void RunShoot(float deltaTime)
     {
-        //AnimatorStateInfo info = _animator.GetCurrentAnimatorStateInfo(0);
+        Move(_shootDirection, SHOOT_SPEED, deltaTime);
+
+        if (IsAnimationCompleted("Shoot"))
+        {
+            TransitionTo(Rise);
+        }
     }
 
     private void BeginRise()
     {
+        _riseTimer = 0.0f;
     }
     
     private void RunRise(float deltaTime)
     {
+        Move(Vector3.up, RISE_SPEED, deltaTime);
+        ClampPositionY(MAX_RISE_Y);
+        _riseTimer += deltaTime;
+        if (_riseTimer > RISE_TIME)
+        {
+            TransitionTo(Shake);
+        }
     }
 
     private void BeginFall()
@@ -67,11 +82,17 @@ public class Bubble : MonoBehaviour
     private void BeginShakeState()
     {
         _shaker.Reset();
+        _shakeTimer = 0.0f;
     }
 
     private void RunShake(float deltaTime)
     {
         ApplyShake();
+        _shakeTimer += deltaTime;
+        if (_shakeTimer > RISE_TIME)
+        {
+            TransitionTo(AboutTopPop);
+        }
     }
 
     private void BeginAboutToPopState()
@@ -82,11 +103,29 @@ public class Bubble : MonoBehaviour
     private void RunAboutToPop(float deltaTime)
     {
         ApplyShake();
+        _aboutToPopTimer += deltaTime;
+        if (_aboutToPopTimer > RISE_TIME)
+        {
+            TransitionTo(Pop);
+        }
     }
 
     private void BeginPop()
     {
         _animator.SetBool("Pop", true);
+    }
+
+    private void RunPop(float deltaTime)
+    {
+        if (IsAnimationCompleted("Pop"))
+        {
+            TransitionTo(Die);
+        }
+    }
+
+    private void BeginDie()
+    {
+        GameObject.Destroy(this.gameObject);
     }
 
     private void CreateStateMachine()
@@ -96,7 +135,8 @@ public class Bubble : MonoBehaviour
         Fall = new State(BeginFall, RunFall, null);
         Shake = new State(BeginShakeState, RunShake, null);
         AboutTopPop = new State(BeginAboutToPopState, RunAboutToPop, null);
-        Pop = new State(BeginPop, null, null);
+        Pop = new State(BeginPop, RunPop, null);
+        Die = new State(BeginDie, null, null);
     }
 
     private State Shoot;
@@ -105,16 +145,37 @@ public class Bubble : MonoBehaviour
     private State Shake;
     private State AboutTopPop;
     private State Pop;
-
+    private State Die;
 
     #endregion Statemachine
 
+    #region AnimationAPI
+    private bool IsAnimationCompleted(string name)
+    {
+        AnimatorStateInfo info = _animator.GetCurrentAnimatorStateInfo(0);
+        return info.IsName(name) && info.normalizedTime >= 1.0f;
+    }
+    #endregion
 
     private void ApplyShake()
     {
-        Vector3 position = _sprite.transform.position;
+        Vector3 position = _sprite.localPosition;
         position.y = _shaker.Value;
-        _sprite.position = position;
+        _sprite.localPosition = position;
+    }
+
+    private void Move(Vector3 direction, float speed, float deltaTime)
+    {
+        Vector3 position = transform.position;
+        position += direction * (deltaTime * speed);
+        transform.position = position;
+    }
+
+    private void ClampPositionY(float max)
+    {
+        Vector3 position = transform.position;
+        position.y = Mathf.Min(max, position.y);
+        transform.position = position;
     }
 
     [SerializeField]
@@ -126,13 +187,32 @@ public class Bubble : MonoBehaviour
     [SerializeField]
     private Animator _animator;
 
+    [SerializeField]
+    private float _currentAnimationTime;
+
     private bool _water; // Is it a water bubble.
+    private Vector3 _shootDirection = new Vector3(1.0f, 0.0f, 0.0f);
 
     private State _currentState;
+    private float _riseTimer;
+    private float _shakeTimer;
+    private float _aboutToPopTimer;
 
     // Constants
+    [SerializeField]
     private Color GREEN_COLOR = Color.green;
+    [SerializeField]
     private Color BLUE_COLOR = Color.blue;
+    [SerializeField]
     private Color NEUTRAL_COLOR = Color.white;
+    [SerializeField]
     private Color ABOUT_TO_POP_COLOR = Color.red;
+    [SerializeField]
+    private float SHOOT_SPEED;
+    [SerializeField]
+    private float RISE_SPEED;
+    [SerializeField]
+    private float MAX_RISE_Y = 1.8f;
+    [SerializeField]
+    private float RISE_TIME = 10.0f;
 }
