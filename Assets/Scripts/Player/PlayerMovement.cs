af440b24cd40975b6a53c6ec1847439b059b3727
Blake Bouchard <thatsblake@gmail.com>
2014-06-21 11:12:10 -0600

    Better movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
     public float horizontalAcceleration = 1.0f;
+    public float horizontalVelocity = 5.0f;
+
+    public bool forceBased = false;
 
 	// Use this for initialization
 	void Start()
@@ -16,6 +19,13 @@ public class PlayerMovement : MonoBehaviour {
     {
         float horizontalAxis = Input.GetAxis("Horizontal");
         //Debug.Log(horizontalAxis);
-        rigidbody2D.AddForce(new Vector2(horizontalAxis * horizontalAcceleration, 0));
+        if (forceBased)
+        {
+            rigidbody2D.AddForce(new Vector2(horizontalAxis * horizontalAcceleration, 0));
+        }
+        else
+        {
+            rigidbody2D.velocity = new Vector2(horizontalAxis * horizontalVelocity, 0);
+        }
 	}
 }
