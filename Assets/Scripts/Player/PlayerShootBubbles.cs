9828e7a82492975a7bed0b6a537881a0c7194eca
Blake Bouchard <thatsblake@gmail.com>
2014-06-21 12:16:16 -0600

    Moving stuff around

---
+++ Assets/Scripts/Player/PlayerShootBubbles.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShootBubbles : MonoBehaviour {
+
+    public Transform bubblePrefab;
+    public float timeBetweenShots = 0.02f;
+    private float timeSinceLastShot = 0;
+
+	// Use this for initialization
+	void Start()
+    {
+	
+	}
+
+    // Create a bubble object at the character
+    private void ShootBubble()
+    {
+        if (bubblePrefab)
+        {
+            Transform bubbleObject = Instantiate(bubblePrefab) as Transform;
+            bubbleObject.name = bubblePrefab.name;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update()
+    {
+	    if (Input.GetButtonDown("Fire1"))
+        {
+            ShootBubble();
+        }
+	}
+}
